08897e8442efd525a9f6cb205165c35791f9b781
Cristyan Lisboa <77303268+Cristyan14@users.noreply.github.com>
2025-08-07 10:48:23 -0300

    Commit 01

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia(numero: "99999-0000", modelo: "Nokia Tijolão", imei: "123456789", memoria: 32);
+        Console.WriteLine("\n--- Testando Nokia ---");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Smartphone iphone = new Iphone(numero: "98888-1234", modelo: "iPhone 13", imei: "987654321", memoria: 128);
+        Console.WriteLine("\n--- Testando iPhone ---");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
